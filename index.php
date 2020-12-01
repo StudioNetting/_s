ceb31f2990b7df95947b49b37f8b9770c2d3e187
Emely Bakke <>
2020-12-01 16:49:03 +0100

    Added a starter .gitignore-file

--- index.php
+++ index.php
@@ -1,41 +1,17 @@
 <?php
 /**
- * The main template file
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package sn
+ * @package WordPress
  */
 
-get_header();
-?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-	<?php if ( have_posts() ) :
-
-		if ( is_home() && ! is_front_page() ) : ?>
-			<header>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-			</header>
-		<?php endif;
-
-		/* Start the Loop */
-		while ( have_posts() ) :
-			the_post();
-			get_template_part( 'modules/featured', get_post_type() );
-		endwhile;
-
-		the_posts_navigation();
-
-	else :
-		get_template_part( 'modules/content', 'none' );
-		
-	endif;
-	?>
-
-<?php
-get_footer();
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
