d4b1b1f756ddcb66b5d99b3dc46446e3b9a338b1
Trine Færaas <trine@Trines-MacBook-Pro-2.local>
2020-12-04 10:02:05 +0100

    Update 404 to Wp standard and remove widgets

--- wp-content/themes/_sn/404.php
+++ wp-content/themes/_sn/404.php
@@ -14,38 +14,12 @@ get_header();
 	<header class="page-header">
 		<h1 class="page-title"><?php esc_html_e( 'Oops, her må det ha skjedd en feil!', 'sn' ); ?></h1>
 	</header>
-	
+
 	<div class="page-content">
 		<p><?php esc_html_e( 'Error 404: Vi kan ikke finne siden du leter etter. Dette kan skyldes at siden er flyttet, har fått ny adresse eller ikke finnes. Kanskje du finner siden ved et søk?', 'sn' ); ?></p>
 
-		<?php
-		get_search_form();
+		<?php get_search_form(); ?>
 
-		the_widget( 'WP_Widget_Recent_Posts' );
-		?>
-
-		<div class="widget widget_categories">
-			<h2 class="widget-title"><?php esc_html_e( 'Mest brukte kategorier:', 'sn' ); ?></h2>
-			<ul>
-				<?php
-				wp_list_categories( array(
-					'orderby'    => 'count',
-					'order'      => 'DESC',
-					'show_count' => 1,
-					'title_li'   => '',
-					'number'     => 10,
-				) );
-				?>
-			</ul>
-		</div>
-
-		<div class="widget widget_archive">
-			<p><?php echo __('Du kan også lete gjennom månedlige arkiver:', 'sn'); ?></p>
-			<?php
-				the_widget( 'WP_Widget_Archives', 'dropdown=1' );
-				the_widget( 'WP_Widget_Tag_Cloud' );
-			?>
-		</div>
 	</div>
 </section>
 
