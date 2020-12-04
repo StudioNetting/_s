3071e8edb7db2164c24febbaab351f7e8cc51140
Trine Færaas <trine@Trines-MacBook-Pro-2.local>
2020-12-04 09:54:29 +0100

    Update page template to WP standard

--- wp-content/themes/_sn/page.php
+++ wp-content/themes/_sn/page.php
@@ -12,25 +12,26 @@
  * @package sn
  */
 
-get_header(); ?>
+get_header();
 
-<?php while ( have_posts() ) : the_post(); ?>
+while ( have_posts() ) :
+	the_post();
+	?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header>
 			<h1><?php the_title(); ?></h1>
 		</header>
-	
+
 		<?php the_content(); ?>	
 	</article>
 
-	<?php // If comments are open or we have at least one comment, load up the comment template.
+	<?php
+	// If comments are open or we have at least one comment, load up the comment template.
 	if ( comments_open() || get_comments_number() ) :
 		comments_template();
 	endif;
 
 endwhile; // End of the loop.
-?>	
 
-<?php
 get_footer();
