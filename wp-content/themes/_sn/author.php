63f2703a7130d59c011a3daf3decca76a8b25f3e
Trine Færaas <trine@Trines-MacBook-Pro-2.local>
2021-01-11 15:29:54 +0100

    Lint author.php

--- wp-content/themes/_sn/author.php
+++ wp-content/themes/_sn/author.php
@@ -8,32 +8,40 @@
  */
 
 get_header();
+
+$get_author = filter_input( INPUT_GET, 'author_name', FILTER_SANITIZE_STRING );
+$curauth    = ( isset( $get_author ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
 ?>
 
 <article>
 
 	<header>
-    <?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
-    	<h1><?php echo __('Om', 'sn'). ' ' .$curauth->nickname ; ?></h1>
+		<h1><?php echo esc_html__( 'Om', 'sn' ) . ' ' . esc_html( $curauth->nickname ); ?></h1>
 	</header>
-	
-	<p><?php echo $curauth->user_description; ?></p>
-    
-    <?php if($curauth->user_url !== ''): ?>
-	    <p>
-	        <span><?php echo __('Nettside:', 'sn'); ?></span>
-	        <a href="<?php echo $curauth->user_url; ?>"><?php echo $curauth->user_url; ?></a>
-	    </p>   
-    <?php endif; ?> 
+	<p><?php echo wp_kses_post( $curauth->user_description ); ?></p>
 
-    <?php if ( have_posts() ) : ?>
-    	<h2><?php echo __('Innlegg skrevet av', 'sn'). ' ' .$curauth->nickname ; ?></h2>
+	<?php
+	if ( $curauth->user_url !== '' ) :
+		?>
+		<p>
+			<span><?php esc_html_e( 'Nettside:', 'sn' ); ?></span>
+			<a href="<?php echo esc_url( $curauth->user_url ); ?>"><?php echo esc_url( $curauth->user_url ); ?></a>
+		</p>   
+		<?php
+	endif;
 
-		<?php while ( have_posts() ) : the_post();
-        	get_template_part( 'modules/featured', get_post_type() );
-		endwhile; 
-		
-    endif; ?>
+	if ( have_posts() ) :
+		?>
+		<h2><?php echo esc_html__( 'Innlegg skrevet av', 'sn' ) . ' ' . esc_html( $curauth->nickname ); ?></h2>
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'modules/featured', get_post_type() );
+		endwhile;
+
+	endif;
+	?>
 
 </article>
 
